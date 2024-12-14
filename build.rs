edf5a771304b7077b4b61cfbc75d408aaccdd8c6
Yehowshua Immanuel <yehowshua@joyofhardware.com>
2024-12-13 20:13:14 -0500

    we should now be able to load in libvterm at runtime

--- build.rs
+++ build.rs
@@ -1,3 +1,38 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     pkg_config::probe_library("sdl2").unwrap();
+
+    // Tell cargo to tell rustc to link the libvterm library
+    println!("cargo:rustc-link-lib=vterm");
+
+    let lib_path = if cfg!(target_arch = "aarch64") {
+        "/opt/homebrew/lib"
+    } else {
+        "/usr/local/lib"
+    };
+    println!("cargo:rustc-link-search={}", lib_path);
+
+    // Determine the correct include path based on the architecture
+    let include_path = if cfg!(target_arch = "aarch64") {
+        // Apple Silicon Macs
+        "-I/opt/homebrew/include"
+    } else {
+        // Intel Macs
+        "-I/usr/local/include"
+    };
+
+    // Add the appropriate include path to the clang argument
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg(include_path) // Use the conditional include path here
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
---
+++ wrapper.h
@@ -0,0 +1,2 @@
+// wrapper.h
+#include <vterm.h>
