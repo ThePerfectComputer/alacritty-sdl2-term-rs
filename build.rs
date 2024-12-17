30514e1348eb8fde6c5a227767d8ae1533641b2d
Yehowshua Immanuel <yehowshua@joyofhardware.com>
2024-12-17 10:36:14 -0500

    remove vterm specific dependencies

--- build.rs
+++ build.rs
@@ -13,16 +13,6 @@ fn main() {
         "-I/usr/local/include"
     };
 
-    // Add the appropriate include path to the clang argument
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg(include_path) // Use the conditional include path here
-        .generate()
-        .expect("Unable to generate bindings");
-
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
